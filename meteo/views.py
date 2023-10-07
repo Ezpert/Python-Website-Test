370434f4d8364dbd101b5342994a2abc68535a33
Ezpert <109883612+Ezpert@users.noreply.github.com>
2023-10-07 14:58:54 -0500

    weatherAPp in python

---
+++ meteo/views.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+import geocoder
+import requests
+from django.http import HttpResponse
+from django.template import loader
+from meteo.models import Worldcities
+
+
+# Create your views here.
+
+
+def temp_here(request):
+    location = geocoder.ip('me').latlng
+    temp = get_temp(location)
+    template = loader.get_template('index.html')
+    context = {'temp':temp}
+    return HttpResponse(template.render(context, request))
+
+
+def get_temp(location):
+    endpoint = "https://api.open-meteo.com/v1/forecast"
+    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m&temperature_unit=fahrenheit"
+    now = datetime.now()
+    hour = now.hour
+    meteo_data = requests.get(api_request).json()
+    temp = meteo_data['hourly']['temperature_2m'][hour]
+    return temp
+
